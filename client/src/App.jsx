f835647bece1c79c0aab4fd8da1256a08f00a18a
rashidpbi <rashidpbi786@gmail.com>
2024-10-21 15:19:05 +0530

    register login pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Routes,Route } from "react-router-dom";
-import IndexPage from "./pages/indexPage";
 import LoginPage from "./pages/LoginPage";
+import Layout from "./components/Layout";
+import IndexPage from "./pages/indexPage";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     
      
       <Routes>
-         <Route path="/" element={<IndexPage />}/>
-         <Route path="/login" element={<LoginPage/>}/>
+       <Route path="/" element={<Layout/>} >
+       <Route index element={<IndexPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+       
+      
+       </Route>
+         
       </Routes>
      
    
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md  mx-auto   ">
+          <input type="text" placeholder="john doe" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
-          <div className="text-center my-2 text-gray-500">Don't have an account?  <Link to={'/register'} className="underline text-black">Register</Link></div>
+          <button className="primary">Register</button>
+          <div className="text-center my-2 text-gray-500">Already a member?  <Link to={'/login'} className="underline text-black">Login</Link></div>
         </form>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
